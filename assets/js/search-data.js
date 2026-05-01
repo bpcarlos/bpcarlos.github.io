0bd8eeb4f0f4ac6a4b4440ece0e4c5e22310595c
bpcarlos <charl_ap@hotmail.es>
2026-05-01 08:18:42 +0000

    Deploying to gh-pages from @ bpcarlos/bpcarlos.github.io@9fdbb50d4d095f9b40d0ee9e4a1eb5b6fdde75a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,8 +406,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-work-a-pac-bayesian-approach-to-generalization-for-quantum-models-is-now-on-arxiv-page-facing-up",
+          title: 'Our work A PAC-Bayesian approach to generalization for quantum models is now on...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
